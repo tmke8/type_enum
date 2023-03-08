9c453daa2b4e2dc1e76baec541f6c8dfb1835afa
Thomas MK <tmke8@posteo.net>
2023-03-08 13:41:35 +0100

    Format with black

--- type-enum/tests/common.py
+++ type-enum/tests/common.py
@@ -7,13 +7,17 @@ _ClassInfo: TypeAlias = type | tuple[type, ...] | types.UnionType
 
 
 class CustomTestCase(TestCase):
-    def assertIsSubclass(self, cls: type, parent_cls: _ClassInfo, msg: Any = None) -> None:
+    def assertIsSubclass(
+        self, cls: type, parent_cls: _ClassInfo, msg: Any = None
+    ) -> None:
         """Same as self.assertTrue(issubclass(cls, parent_cls))."""
         if not issubclass(cls, parent_cls):
             standardMsg = "%r is not a subclass of %r" % (cls, parent_cls)
             self.fail(self._formatMessage(msg, standardMsg))
 
-    def assertNotIsSubclass(self, cls: type, parent_cls: _ClassInfo, msg: Any = None) -> None:
+    def assertNotIsSubclass(
+        self, cls: type, parent_cls: _ClassInfo, msg: Any = None
+    ) -> None:
         """Included for symmetry with assertIsSubclass."""
         if issubclass(cls, parent_cls):
             standardMsg = "%r is a subclass of %r" % (cls, parent_cls)
