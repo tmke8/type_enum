826639144195e2f0f9bf9e3922062b15ec9db6f1
Thomas M Kehrenberg <tmke8@posteo.net>
2024-02-23 16:44:23 +0100

    Address ruff's lints

--- type-enum/example.py
+++ type-enum/example.py
@@ -1,5 +1,7 @@
 from typing import Final
+
 from type_enum import TypeEnum
+
 
 class BgColor(TypeEnum):
     transparent = ()
@@ -7,6 +9,8 @@ class BgColor(TypeEnum):
     name = (str,)
     rgb = {"red": int, "green": int, "blue": int}  # named args
     hsv = {"hue": int, "saturation": int, "value": int}
+
+
 reveal_type(BgColor.rgb)
 background_color = BgColor.rgb(red=39, green=127, blue=168)
 reveal_type(background_color.red)
@@ -25,5 +29,6 @@ def f(color: BgColor) -> None:
             print(f"RGB: {r}, {g}, {b}")
         case BgColor.hsv(hue=h, saturation=s, value=v):
             print(f"HSV: {h}, {s}, {v}")
+
 
 f(background_color)
